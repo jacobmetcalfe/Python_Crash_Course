d7d68c30b627fd31d8a8f738409f5a56b40b3110
Jacob_Metcalfe <46358142+jacobmetcalfe@users.noreply.github.com>
2019-02-06 14:55:18 -0700

    Update and rename Chapter 1.py to Chapter 1: Get Python.py

--- Chapter 1: Get Python.py
+++ Chapter 1: Get Python.py
@@ -4,3 +4,4 @@
 # Chapter 1, Intro just contained how to set python up
 # You really just go on the Python website, download the latest version
 # I would recommend an IDE rather than terminal, or even just an online compiler
+# If student, Pycharm from JetBrains is great.
