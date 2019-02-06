e21fff40c598cc2c6d2687d3aecc992c3a0271d8
Jacob_Metcalfe <46358142+jacobmetcalfe@users.noreply.github.com>
2019-02-06 13:18:55 -0700

    Add files via upload

---
+++ Chapter 1.py
@@ -0,0 +1,6 @@
+# Information from 'Python Crash Course, A Hands-on, Project - Based Introduction to programming
+# By Eric Matthes
+
+# Chapter 1, Intro just contained how to set python up
+# You really just go on the Python website, download the latest version
+# I would recommend an IDE rather than terminal, or even just an online compiler
